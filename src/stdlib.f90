edbdc72e052ead24d5b8d543b1b344e1bec20d6c
Vladimir Terzi <vladimir.terzi@rwth-aachen.de>
2026-06-07 14:32:04 +0200

    Move old files to a separate folder

--- src/stdlib.f90
+++ src/stdlib.f90
@@ -1,14 +1,5 @@
 module stdlib
     use stdlib_kinds
-    use stdlib_base
-    use stdlib_math
-    use stdlib_string
-    use stdlib_array
-    use stdlib_unittest
-    use stdlib_list
-    use stdlib_io
-    use stdlib_time
-    use stdlib_subprocess
 
     implicit none
 end module stdlib
