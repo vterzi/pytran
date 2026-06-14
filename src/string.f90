1321d3bfe08bceb19822ada0f5cee4b73078f62a
Vladimir Terzi <vladimir.terzi@rwth-aachen.de>
2026-06-14 18:14:47 +0200

    Add some procedures to string.f90

--- src/string.f90
+++ src/string.f90
@@ -53,4 +53,233 @@ module pytran_string
             // SYN // ETB // CAN // EM // SUB // ESC // FS // GS // RS // US &
             // DEL, &
         WORDCHARS = DIGITS // ASCII_LETTERS // '_'
+
+    integer, parameter :: &
+        CASESHIFT = iachar(ASCII_LOWERCASE(:1)) - iachar(ASCII_UPPERCASE(:1))
+
+contains
+    elemental function isspace(arg) result(res)
+        character(len=*), intent(in) :: arg
+        logical :: res
+
+        res = len(arg) > 0 .and. verify(arg, WHITESPACE) == 0
+    end function isspace
+
+
+    elemental function isdigit(arg) result(res)
+        character(len=*), intent(in) :: arg
+        logical :: res
+
+        res = len(arg) > 0 .and. verify(arg, DIGITS) == 0
+    end function isdigit
+
+
+    elemental function isalpha(arg) result(res)
+        character(len=*), intent(in) :: arg
+        logical :: res
+
+        res = len(arg) > 0 .and. verify(arg, ASCII_LETTERS) == 0
+    end function isalpha
+
+
+    elemental function isalnum(arg) result(res)
+        character(len=*), intent(in) :: arg
+        logical :: res
+
+        res = len(arg) > 0 .and. verify(arg, DIGITS // ASCII_LETTERS) == 0
+    end function isalnum
+
+
+    elemental function isidentifier(arg) result(res)
+        character(len=*), intent(in) :: arg
+        logical :: res
+
+        res = len(arg) > 0 &
+            .and. verify(arg, WORDCHARS) == 0 &
+            .and. scan(arg(1:1), DIGITS // '_') == 0
+    end function isidentifier
+
+
+    elemental function isprintable(arg) result(res)
+        character(len=*), intent(in) :: arg
+        logical :: res
+
+        res = verify(arg, PRINTABLE) == 0
+    end function isprintable
+
+
+    elemental function isascii(arg) result(res)
+        character(len=*), intent(in) :: arg
+        logical :: res
+
+        res = verify(arg, PRINTABLE // NONPRINTABLE) == 0
+    end function isascii
+
+
+    elemental function isupper(arg) result(res)
+        character(len=*), intent(in) :: arg
+        logical :: res
+
+        res = ( &
+            scan(arg, ASCII_UPPERCASE) > 0 &
+            .and. verify(arg, ASCII_LOWERCASE) == 0 &
+        )
+    end function isupper
+
+
+    elemental function islower(arg) result(res)
+        character(len=*), intent(in) :: arg
+        logical :: res
+
+        res = ( &
+            scan(arg, ASCII_LOWERCASE) > 0 &
+            .and. verify(arg, ASCII_UPPERCASE) == 0 &
+        )
+    end function islower
+
+
+    elemental function upper(arg) result(res)
+        character(len=*), intent(in) :: arg
+        character(len=len(arg)) :: res
+
+        integer :: i
+
+        res = arg
+        i = 0
+        do
+            i = scan(res(i + 1 :), ASCII_LOWERCASE)
+            if (i == 0) exit
+            res(i:i) = achar(iachar(res(i:i)) - CASESHIFT)
+        end do
+    end function upper
+
+
+    elemental function lower(arg) result(res)
+        character(len=*), intent(in) :: arg
+        character(len=len(arg)) :: res
+
+        integer :: i
+
+        res = arg
+        i = 0
+        do
+            i = scan(res(i + 1 :), ASCII_UPPERCASE)
+            if (i == 0) exit
+            res(i:i) = achar(iachar(res(i:i)) + CASESHIFT)
+        end do
+    end function lower
+
+
+    elemental function is_logical(arg) result(res)
+        character(len=*), intent(in) :: arg
+        logical :: res
+
+        integer :: n, i
+
+        n = len(arg)
+        res = n > 0
+        if (.not. res) return
+        i = 1
+        if (arg(i:i) == '.') i = i + 1
+        res = i <= n
+        if (.not. res) return
+        res = verify(arg(i:i), 'TtFf') == 0
+    end function is_logical
+
+
+    elemental function is_sign(arg) result(res)
+        character(len=*), intent(in) :: arg
+        logical :: res
+
+        res = arg == '+' .or. arg == '-'
+    end function is_sign
+
+
+    elemental function is_unsigned_integer(arg) result(res)
+        character(len=*), intent(in) :: arg
+        logical :: res
+
+        res = len(arg) > 0 .and. verify(arg, DIGITS) == 0
+    end function is_unsigned_integer
+
+
+    elemental function is_integer(arg) result(res)
+        character(len=*), intent(in) :: arg
+        logical :: res
+
+        integer :: i
+
+        res = len(arg) > 0
+        if (.not. res) return
+        i = 1
+        if (is_sign(arg(i:i))) i = i + 1
+        res = is_unsigned_integer(arg(i:))
+    end function is_integer
+
+
+    elemental function is_unsigned_fixed_real(arg) result(res)
+        character(len=*), intent(in) :: arg
+        logical :: res
+
+        integer :: i
+
+        i = index(arg, '.')
+        if (i > 0) then
+            res = is_unsigned_integer(arg(: i - 1) // arg(i + 1 :))
+        else
+            res = is_unsigned_integer(arg)
+        end if
+    end function is_unsigned_fixed_real
+
+
+    elemental function is_fixed_real(arg) result(res)
+        character(len=*), intent(in) :: arg
+        logical :: res
+
+        integer :: i
+
+        res = len(arg) > 0
+        if (.not. res) return
+        i = 1
+        if (is_sign(arg(i:i))) i = i + 1
+        res = is_unsigned_fixed_real(arg(i:))
+    end function is_fixed_real
+
+
+    elemental function is_unsigned_float_real(arg) result(res)
+        character(len=*), intent(in) :: arg
+        logical :: res
+
+        integer :: i
+        character(len=len(arg)) :: temp
+
+        temp = lower(arg)
+        if (temp == 'nan' .or. temp == 'inf' .or. temp == 'infinity') then
+            res = .true.
+        else
+            i = scan(arg, 'EeDd')
+            if (i > 0) then
+                res = ( &
+                    is_unsigned_fixed_real(arg(: i - 1)) &
+                    .and. is_integer(arg(i + 1 :)) &
+                )
+            else
+                res = is_unsigned_fixed_real(arg)
+            end if
+        end if
+    end function is_unsigned_float_real
+
+
+    elemental function is_float_real(arg) result(res)
+        character(len=*), intent(in) :: arg
+        logical :: res
+
+        integer :: i
+
+        res = len(arg) > 0
+        if (.not. res) return
+        i = 1
+        if (is_sign(arg(i:i))) i = i + 1
+        res = is_unsigned_float_real(arg(i:))
+    end function is_float_real
 end module pytran_string
