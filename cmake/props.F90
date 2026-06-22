47fc821f2a9c1367a25b4d780279dc0317208bff
Vladimir Terzi <vladimir.terzi@rwth-aachen.de>
2026-06-22 02:02:56 +0200

    Use ieee_arithmetic in props.F90

--- cmake/props.F90
+++ cmake/props.F90
@@ -1,23 +1,10 @@
 program props
+    use, intrinsic :: ieee_arithmetic, only: &
+        ieee_value, ieee_quiet_nan, ieee_positive_inf
+
     implicit none
 
     integer :: n
-    real :: r
-#ifdef _HP
-    real(_HP) :: rhp
-#endif
-#ifdef _SP
-    real(_SP) :: rsp
-#endif
-#ifdef _DP
-    real(_DP) :: rdp
-#endif
-#ifdef _XDP
-    real(_XDP) :: rxdp
-#endif
-#ifdef _QP
-    real(_QP) :: rqp
-#endif
 
     print *, "-huge(0)-1=", -huge(0) - 1
 
@@ -92,18 +79,17 @@ program props
     print *, "`"
 #endif
 
+    n = (storage_size(0.) - 1) / storage_size(0) + 1
     print *, "REAL:"
     print *, "SIZE=", storage_size(0.) / 8
     print *, "STR=`"
     print *, -huge(0.)
     print *, "`"
-    r = 0
-    n = (storage_size(r) - 1) / storage_size(0) + 1
     print *, "INF=`"
-    print *, transfer(1 / r, 0, n)
+    print *, transfer(ieee_value(0., ieee_positive_inf), 0, n)
     print *, "`"
     print *, "NAN=`"
-    print *, transfer(0 / r, 0, n)
+    print *, transfer(ieee_value(0., ieee_quiet_nan), 0, n)
     print *, "`"
     print *, "COMPLEX:"
     print *, "SIZE=", storage_size((0, 0)) / 8
@@ -111,18 +97,17 @@ program props
     print *, cmplx(-huge(0.), -huge(0.))
     print *, "`"
 #ifdef _HP
+    n = (storage_size(real(0, _HP)) - 1) / storage_size(0) + 1
     print *, "REAL_HP:"
     print *, "SIZE=", storage_size(real(0, _HP)) / 8
     print *, "STR=`"
     print *, -huge(real(0, _HP))
     print *, "`"
-    rhp = 0
-    n = (storage_size(rhp) - 1) / storage_size(0) + 1
     print *, "INF=`"
-    print *, transfer(1 / rhp, 0, n)
+    print *, transfer(ieee_value(real(0, _HP), ieee_positive_inf), 0, n)
     print *, "`"
     print *, "NAN=`"
-    print *, transfer(0 / rhp, 0, n)
+    print *, transfer(ieee_value(real(0, _HP), ieee_quiet_nan), 0, n)
     print *, "`"
     print *, "COMPLEX_HP:"
     print *, "SIZE=", storage_size(cmplx(0, 0, _HP)) / 8
@@ -131,18 +116,17 @@ program props
     print *, "`"
 #endif
 #ifdef _SP
+    n = (storage_size(real(0, _SP)) - 1) / storage_size(0) + 1
     print *, "REAL_SP:"
     print *, "SIZE=", storage_size(real(0, _SP)) / 8
     print *, "STR=`"
     print *, -huge(real(0, _SP))
     print *, "`"
-    rsp = 0
-    n = (storage_size(rsp) - 1) / storage_size(0) + 1
     print *, "INF=`"
-    print *, transfer(1 / rsp, 0, n)
+    print *, transfer(ieee_value(real(0, _SP), ieee_positive_inf), 0, n)
     print *, "`"
     print *, "NAN=`"
-    print *, transfer(0 / rsp, 0, n)
+    print *, transfer(ieee_value(real(0, _SP), ieee_quiet_nan), 0, n)
     print *, "`"
     print *, "COMPLEX_SP:"
     print *, "SIZE=", storage_size(cmplx(0, 0, _SP)) / 8
@@ -151,18 +135,17 @@ program props
     print *, "`"
 #endif
 #ifdef _DP
+    n = (storage_size(real(0, _DP)) - 1) / storage_size(0) + 1
     print *, "REAL_DP:"
     print *, "SIZE=", storage_size(real(0, _DP)) / 8
     print *, "STR=`"
     print *, -huge(real(0, _DP))
     print *, "`"
-    rdp = 0
-    n = (storage_size(rdp) - 1) / storage_size(0) + 1
     print *, "INF=`"
-    print *, transfer(1 / rdp, 0, n)
+    print *, transfer(ieee_value(real(0, _DP), ieee_positive_inf), 0, n)
     print *, "`"
     print *, "NAN=`"
-    print *, transfer(0 / rdp, 0, n)
+    print *, transfer(ieee_value(real(0, _DP), ieee_quiet_nan), 0, n)
     print *, "`"
     print *, "COMPLEX_DP:"
     print *, "SIZE=", storage_size(cmplx(0, 0, _DP)) / 8
@@ -171,18 +154,17 @@ program props
     print *, "`"
 #endif
 #ifdef _XDP
+    n = (storage_size(real(0, _XDP)) - 1) / storage_size(0) + 1
     print *, "REAL_XDP:"
     print *, "SIZE=", storage_size(real(0, _XDP)) / 8
     print *, "STR=`"
     print *, -huge(real(0, _XDP))
     print *, "`"
-    rxdp = 0
-    n = (storage_size(rxdp) - 1) / storage_size(0) + 1
     print *, "INF=`"
-    print *, transfer(1 / rxdp, 0, n)
+    print *, transfer(ieee_value(real(0, _XDP), ieee_positive_inf), 0, n)
     print *, "`"
     print *, "NAN=`"
-    print *, transfer(0 / rxdp, 0, n)
+    print *, transfer(ieee_value(real(0, _XDP), ieee_quiet_nan), 0, n)
     print *, "`"
     print *, "COMPLEX_XDP:"
     print *, "SIZE=", storage_size(cmplx(0, 0, _XDP)) / 8
@@ -191,18 +173,17 @@ program props
     print *, "`"
 #endif
 #ifdef _QP
+    n = (storage_size(real(0, _QP)) - 1) / storage_size(0) + 1
     print *, "REAL_QP:"
     print *, "SIZE=", storage_size(real(0, _QP)) / 8
     print *, "STR=`"
     print *, -huge(real(0, _QP))
     print *, "`"
-    rqp = 0
-    n = (storage_size(rqp) - 1) / storage_size(0) + 1
     print *, "INF=`"
-    print *, transfer(1 / rqp, 0, n)
+    print *, transfer(ieee_value(real(0, _QP), ieee_positive_inf), 0, n)
     print *, "`"
     print *, "NAN=`"
-    print *, transfer(0 / rqp, 0, n)
+    print *, transfer(ieee_value(real(0, _QP), ieee_quiet_nan), 0, n)
     print *, "`"
     print *, "COMPLEX_QP:"
     print *, "SIZE=", storage_size(cmplx(0, 0, _QP)) / 8
