b21151c589a6cfcfa1e317d7de12599339796e31
Vladimir Terzi <vladimir.terzi@rwth-aachen.de>
2026-06-23 17:44:13 +0200

    Add inf and nan

--- src/math/__init__.F90
+++ src/math/__init__.F90
@@ -4,38 +4,20 @@
 module pytran_math
     use, intrinsic :: ieee_arithmetic, only: ieee_is_finite, ieee_is_nan
     use :: pytran_kinds
+    use :: pytran_number
 
     implicit none
 
     private
 
-#ifdef _HP
-    real(_HP), parameter, public :: &
-        INF_HP = _REAL_HP_INF, &
-        NAN_HP = _REAL_HP_NAN
-#endif
-#ifdef _SP
-    real(_SP), parameter, public :: &
-        INF_SP = _REAL_SP_INF, &
-        NAN_SP = _REAL_SP_NAN
-#endif
-#ifdef _DP
-    real(_DP), parameter, public :: &
-        INF_DP = _REAL_DP_INF, &
-        NAN_DP = _REAL_DP_NAN
-#endif
-#ifdef _XDP
-    real(_XDP), parameter, public :: &
-        INF_XDP = _REAL_XDP_INF, &
-        NAN_XDP = _REAL_XDP_NAN
-#endif
-#ifdef _QP
-    real(_QP), parameter, public :: &
-        INF_QP = _REAL_QP_INF, &
-        NAN_QP = _REAL_QP_NAN
-#endif
-
 #define _PROC _CAT2(_OP,_LABEL)
+
+#define _TYPE_IDS _REAL
+#define _OP inf
+#include "../inc/iface.inc"
+#define _OP nan
+#include "../inc/iface.inc"
+#undef _TYPE_IDS
 
 #define _TYPE_IDS (_LOGICAL | _INTEGER | _REAL)
 #define _OP isfinite
@@ -54,6 +36,12 @@ module pytran_math
 #undef _PROC
 
 contains
+
+#define _TYPE_IDS _REAL
+#define _FILE "../math/consts.inc"
+#include "../inc/types.inc"
+#undef _FILE
+#undef _TYPE_IDS
 
 #define _TYPE_IDS (_LOGICAL | _INTEGER | _REAL)
 #define _FILE "../math/ieee.inc"
